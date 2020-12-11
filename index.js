b3e77d2ee9c261ef5554a969df049ebbf2ee8eda
migueljr <miguel_jr@mail.com>
2020-12-11 17:35:44 -0300

    'alter'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions']});
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://github.com/migueljr/botwhatsapptestes');
   await page.screenshot({path: 'example.png'});
